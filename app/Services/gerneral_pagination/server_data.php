e822722ee9fc9168a67621476faf2c688e767eca
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-03-23 02:01:27 +0200

    adding

--- app/Services/gerneral_pagination/server_data.php
+++ app/Services/gerneral_pagination/server_data.php
@@ -38,7 +38,9 @@ class server_data
                                     $e->orWhere($key, '=', 1);
                                 }
                                 if (str_contains( 'تم الارسال للموردين',$value) == true || str_contains( 'sent to vendors',$value) == true) {
-                                    $e->orWhere($key, '=', 0)->whereDoesntHave('items',function($q){
+                                    $e->orWhere($key, '=', 0)
+                                        ->has('vendors_requests')
+                                        ->whereDoesntHave('items',function($q){
                                         $q->whereHas('user',function($u){
                                            $u->whereHas('role',function($r){
                                                $r->where('name','=','seller');
