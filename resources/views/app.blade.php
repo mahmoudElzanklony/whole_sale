4c7706ba8d52fdda74bbde9f3a340c23ab367026
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-06-05 04:24:45 +0300

    adding

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -51,7 +51,7 @@
 
 
 
-    <script src="{{ mix('/js/app.js') }}?v=77.91.7" defer></script>
+    <script src="{{ mix('/js/app.js') }}?v=79.33.7" defer></script>
     <style>
         /*body{
             overflow: hidden;
